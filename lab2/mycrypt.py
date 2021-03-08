293377503e6d0e327fff32e3748b9eb39b02aebf
Olli <55876794+OMP404@users.noreply.github.com>
2021-03-08 19:09:15 +0200

    Update mycrypt.py

--- lab2/mycrypt.py
+++ lab2/mycrypt.py
@@ -14,6 +14,7 @@ def encode(s):
                 c=c.upper()
             # Rot13 the character for maximum security
             crypted+=codecs.encode(c,'rot13')
+            break
         elif c in digitmapping:
           crypted+=digitmapping[c]
         else:
