8468e2996566669e8499027c08517b567c0bd406
Olli <55876794+OMP404@users.noreply.github.com>
2021-03-09 13:34:34 +0200

    Update mycrypt.py

--- lab2/mycrypt.py
+++ lab2/mycrypt.py
@@ -15,29 +15,26 @@ class ConstantTime():
         self.timer.join()
         
 def encode(s):
-    try:
-        if not isinstance(s,str):
-            raise TypeError
-        origlen = len(s)
-        "{:<999}".format(s)
-        crypted = ""
-        digitmapping = dict(zip('1234567890!"#€%&/()=','!"#€%&/()=1234567890'))
-        if len(s) > 1000:
-            raise ValueError
-        with ConstantTime(0.1):
-            for c in s:
-                if c in ['+', 'å', 'ä','ö']:
-                    raise ValueError
-                elif c.isalpha():
-                    c = c.upper()
-                    crypted += codecs.encode(c, 'rot13')
-                else:
-                    crypted += digitmapping[c]
+    
+    if not isinstance(s,str):
+        raise TypeError
+    origlen = len(s)
+    "{:<999}".format(s)
+    crypted = ""
+    digitmapping = dict(zip('1234567890!"#€%&/()=','!"#€%&/()=1234567890'))
+    if len(s) > 1000:
+        raise ValueError
+    with ConstantTime(0.1):
+        for c in s:
+            if c in ['+', 'å', 'ä','ö']:
+                raise ValueError
+            elif c.isalpha():
+                c = c.upper()
+                crypted += codecs.encode(c, 'rot13')
+            else:
+                crypted += digitmapping[c]
 
-        return crypted[:origlen]
-    except TypeError:
-        pass
-        
+    return crypted[:origlen]
+   
 def decode(s):
     return encode(s).lower()
-
