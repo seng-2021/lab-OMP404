96919e2f5a10dc087c2e968aaddc8768d0887cb4
Olli <55876794+OMP404@users.noreply.github.com>
2021-03-09 12:08:43 +0200

    Update mycrypt.py

--- lab2/mycrypt.py
+++ lab2/mycrypt.py
@@ -1,6 +1,17 @@
 import codecs
 import string
 
+class ConstantTime():
+    def __init__(self, length):
+        self.length = length
+
+    def __enter__(self):
+        self.timer = threading.Thread(target=time.sleep, args=[self.length])
+        self.timer.start()
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.timer.join()
+        
 def encode(s):
     if not isinstance(s,str):
         raise TypeError
@@ -10,14 +21,15 @@ def encode(s):
     digitmapping = dict(zip('1234567890!"#€%&/()=','!"#€%&/()=1234567890'))
     if len(s) > 1000:
         raise ValueError
-    for c in s:
-        if c in ['+', 'å', 'ä','ö']:
-            raise ValueError
-        elif c.isalpha():
-            c = c.upper()
-            crypted += codecs.encode(c, 'rot13')
-        else:
-            crypted += digitmapping[c]
+    with ConstantTime(0.1)
+        for c in s:
+            if c in ['+', 'å', 'ä','ö']:
+                raise ValueError
+            elif c.isalpha():
+                c = c.upper()
+                crypted += codecs.encode(c, 'rot13')
+            else:
+                crypted += digitmapping[c]
             
     return crypted[:origlen]
 
