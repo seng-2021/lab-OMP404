19f450afd1fff4831c900c73b58ba1b7a21035e1
Olli <55876794+OMP404@users.noreply.github.com>
2021-03-09 17:33:14 +0200

    Update mycrypt.py

--- lab2/mycrypt.py
+++ lab2/mycrypt.py
@@ -15,7 +15,7 @@ class ConstantTime():
         self.timer.join()
         
 def encode(s):
-    if s == "":
+    if s == invalid_input:
         raise TypeError
     origlen = len(s)
     "{:<999}".format(s) 
