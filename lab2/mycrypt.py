b8d45f548111f8769360e3dc4db93d457ff07632
Olli <55876794+OMP404@users.noreply.github.com>
2021-03-09 11:48:25 +0200

    Update mycrypt.py

--- lab2/mycrypt.py
+++ lab2/mycrypt.py
@@ -11,14 +11,14 @@ def encode(s):
     if len(s) > 1000:
         raise ValueError
     for c in s:
-        if c.isalpha():
-            c=c.upper()
-            # Rot13 the character for maximum security
-            crypted+=codecs.encode(c,'rot13')
-        elif c in digitmapping:
-          crypted+=digitmapping[c]
-        else:
+        if c in ['+', 'å', 'ä','ö']:
             raise ValueError
+        elif c.isalpha():
+            c = c.upper()
+            crypted += codecs.encode(c, 'rot13')
+        else:
+            crypted += digitmapping[c]
+            
     return crypted[:origlen]
 
 def decode(s):
