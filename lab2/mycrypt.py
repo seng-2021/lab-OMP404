cef5dc76b860092d031e6992086a005f97f44c4a
Olli <55876794+OMP404@users.noreply.github.com>
2021-03-10 11:22:08 +0200

    Update mycrypt.py

--- lab2/mycrypt.py
+++ lab2/mycrypt.py
@@ -25,7 +25,7 @@ def encode(s):
     digitmapping = dict(zip('1234567890!"#€%&/()=','!"#€%&/()=1234567890'))
     with ConstantTime(0.1):
         for c in s:
-            if c in ['å','ä','ö']
+            if c in ['å','ä','ö']:
                 raise ValueError
             if c.isalpha():
                 c = c.upper()
